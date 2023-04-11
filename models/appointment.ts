f881285d36c00db19fc0c106db79052fa9d0614e
TeaPot98 <trifautan@gmail.com>
2023-04-11 20:42:13 +0300

    Added appointment model. Edited patient model.

---
+++ models/appointment.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+import { formatMongoSchema } from "./utils";
+
+const appointmentSchema = new mongoose.Schema({
+  patient: { type: mongoose.Types.ObjectId, ref: "Patient" },
+  author: {
+    name: String,
+    surname: String,
+    id: String,
+  },
+  examinationDate: Date,
+  acuses: String,
+  objectiveExam: String,
+  investigationResults: String,
+  diagnosis: String,
+  recommendations: String,
+});
+
+export const Appointment = mongoose.model(
+  "Patient",
+  formatMongoSchema(appointmentSchema)
+);
--- models/patient.ts
+++ models/patient.ts
@@ -3,24 +3,14 @@ import mongoose from "mongoose";
 import { formatMongoSchema } from "./utils";
 
 const patientSchema = new mongoose.Schema({
+  patientId: mongoose.Types.ObjectId,
   name: String,
   surname: String,
-  author: {
-    name: String,
-    surname: String,
-    id: String,
-  },
   birthDate: Date,
   sex: String,
   address: String,
   idnp: String,
   phone: String,
-  examinationDate: Date,
-  acuses: String,
-  objectiveExam: String,
-  investigationResults: String,
-  diagnosis: String,
-  recommendations: String,
 });
 
 export const Patient = mongoose.model(
