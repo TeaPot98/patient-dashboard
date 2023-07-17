c6b8eee04ff6f2f5b767a7995680e3f44285bcbe
Anastasia Baltă <anastasiabalta@MacBook-Air-Anastasia.local>
2023-07-18 00:20:47 +0300

    Changed default secret to a valid one

--- utils/config.ts
+++ utils/config.ts
@@ -1,3 +1,3 @@
 export const PORT = process.env.PORT || 8080;
 export const MONGODB_URI = process.env.MONGODB_URI || "";
-export const JWT_SECRET = process.env.JWT_SECRET || "";
+export const JWT_SECRET = process.env.JWT_SECRET || "secret";
